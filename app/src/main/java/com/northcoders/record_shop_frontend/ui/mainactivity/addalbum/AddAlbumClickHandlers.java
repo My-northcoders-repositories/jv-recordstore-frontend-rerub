1a9febc0e0e072685266174b0a96fd162001ff70
ludic <notetakingforeggs@gmail.com>
2024-06-21 11:48:07 +0100

    Feat: button logic implemented in addalbumclickhandlers

---
+++ app/src/main/java/com/northcoders/record_shop_frontend/ui/mainactivity/addalbum/AddAlbumClickHandlers.java
@@ -0,0 +1,55 @@
+package com.northcoders.record_shop_frontend.ui.mainactivity.addalbum;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import com.northcoders.record_shop_frontend.MainActivity;
+import com.northcoders.record_shop_frontend.model.Album;
+import com.northcoders.record_shop_frontend.ui.mainactivity.mainactivity.MainActivityViewModel;
+
+public class AddAlbumClickHandlers {
+    private Album album;
+    private Application application;
+    private MainActivityViewModel viewModel;
+
+    public AddAlbumClickHandlers(Album album, Application application, MainActivityViewModel viewModel) {
+        this.album = album;
+        this.application = application;
+        this.viewModel = viewModel;
+    }
+
+    public void createNewAlbum(View view) {
+        if (album.getAlbumTitle().isEmpty()
+                || album.getArtist().isEmpty()
+                || album.getGenre().isEmpty()
+                || album.getPricePence().isEmpty()
+                || album.getReleaseYear().isEmpty()) {
+            Toast.makeText(application.getApplicationContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+        }else{
+
+            Intent intent = new Intent(application.getApplicationContext(), MainActivity.class);
+
+            Album newAlbum = new Album();
+            newAlbum.setAlbumTitle(album.getAlbumTitle());
+            newAlbum.setArtist(album.getArtist());
+            newAlbum.setGenre(album.getGenre());
+            newAlbum.setPricePence(Long.valueOf(album.getPricePence()));
+            newAlbum.setReleaseYear(album.getReleaseYear());
+
+            viewModel.addAlbum(newAlbum);
+            application.getApplicationContext().startActivity(intent);
+
+        }
+
+    }
+
+    public void goBackToMainView(View view) {
+
+        Intent intent = new Intent(application.getApplicationContext(), MainActivity.class);
+        application.getApplicationContext().startActivity(intent);
+
+    }
+
+}
